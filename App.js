bd600e3087ce48dd0c400ecc7adac48093821207
Alessandro Emanuel <alesefs@hotmail.com>
2024-02-21 22:47:04 -0300

    update just files to work timcoo in expo

---
+++ App.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+import styles from './App.styles';
+
+import Slider from '@react-native-community/slider';
+//no package.json -> "@react-native-community/slider": "^2.0.2",
+
+import Emoji from 'react-native-emoji';
+import AsyncStorage from '@react-native-community/async-storage';
+
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+        currrentValue: 50,
+        minCurrrentValue: 0,
+        maxCurrrentValue: 100,
+        mood: ["Muito Triste", "Triste", "Normal", "Feliz", "Muito Feliz"],
+        emoji: ["sob", "disappointed", "neutral_face", "blush", "grin"],
+        backgroundColor: this.colorBackground(0),
+    }
+  }
+
+  componentDidMount = () => {       
+    AsyncStorage.getItem('currrentValue', (err, value) => {
+        if (err) {
+            console.error(err)
+        } else {
+            if (value) {
+              this.setState({ 
+                currrentValue: parseInt(JSON.parse(value), 10),
+                backgroundColor: this.colorBackground(Math.round(parseInt(JSON.parse(value), 10)/25)),
+              });
+            } else {
+              value = 50;
+              this.setState({ 
+                currrentValue: value,
+                backgroundColor: this.colorBackground(value/25),
+              });
+            }
+        }
+    })
+  }
+
+  setcurrrentValue = (value) => {
+    AsyncStorage.setItem('currrentValue', JSON.stringify(value))
+    this.setState({ 
+      currrentValue: value,
+    });
+  }
+
+  render() {
+
+    return (
+      <>
+        <SafeAreaView style={[styles.container, {backgroundColor: this.state.backgroundColor+55}]}>
+
+          {/* efeitos quadrados do background */}
+          <View style={[styles.roundRectEffect, {backgroundColor: this.state.backgroundColor+88}]}/>
+          <View style={[styles.roundRectEffect2, {backgroundColor: this.state.backgroundColor+88}]}/>
+
+          <Text style={styles.title}>MEU HUMOR</Text>
+
+          <View style={styles.centralize}>
+            {/* imagem do humor da pessoa */}
+            <View style={[styles.roundRect, {backgroundColor: this.state.backgroundColor}]}>
+              { this.displayImage() }
+            </View>
+
+            {/* descritivos do humor da pessoa */}
+            <View style={styles.mood}>
+              {/* textos do humor */}
+              <Text style={styles.moodDescription}>
+                { this.displayMessage() }
+              </Text>
+              {/* valor do slider */}
+              <Text style={styles.moodPercent}>
+                {Math.round(this.state.currrentValue)} %
+              </Text>
+            </View>
+
+            {/* slider */}
+            <Slider
+                style={{width:"80%"}} 
+                minimumValue={this.state.minCurrrentValue}
+                maximumValue={this.state.maxCurrrentValue}
+                value={this.state.currrentValue}
+                step={1}
+                onValueChange={val => this.setState({ 
+                  currrentValue: val,
+                  backgroundColor: this.colorBackground(Math.round(val/25)),
+                })}
+                thumbTintColor={'#263238'}
+                maximumTrackTintColor={'#cfd8dc'}
+                minimumTrackTintColor={'#263238'}
+              />
+          </View>
+          
+          {/* botao */}
+          <TouchableOpacity onPress = {() => { this.setcurrrentValue(Math.round(this.state.currrentValue)) }}>
+              <View style={styles.buttonSend}>
+                  <Text style={styles.buttonSendTxt}>SALVAR</Text>
+              </View>
+          </TouchableOpacity>
+      
+        </SafeAreaView>
+      </>
+    );
+  }
+
+  //cores do background
+  colorBackground = (value) => {
+    let colors = ["#ef9a9a", "#ffccbc", "#fff59d", "#c8e6c9", "#81d4fa"];
+    return colors[value];
+  }
+
+  //emojis de humor
+  displayImage = () => {
+    return <Emoji name={this.state.emoji[Math.round(this.state.currrentValue/25)]} style={styles.emoji} />;
+  }
+
+  //texto do humor
+  displayMessage = () => {
+    return `${(this.state.mood[Math.round(this.state.currrentValue/25)]).toLocaleUpperCase()}`;
+  }
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# TestTimcooReactNative
-teste em react native para o applicativo timcoo em 2020
+# TestTimcooReactNative2020
+teste em react native para a empresa timcoo 2020
+
+---
+
+> use o expo para testar
+
+Open the `App.js` file to start writing some code. You can preview the changes directly on your phone or tablet by scanning the **QR code** or use the iOS or Android emulators. When you're done, click **Save** and share the link!
+
+When you're ready to see everything that Expo provides (or if you want to use your own editor) you can **Download** your project and use it with [expo cli](https://docs.expo.dev/get-started/installation/#expo-cli)).
+
+All projects created in Snack are publicly available, so you can easily share the link to this project via link, or embed it on a web page with the `<>` button.
+
+If you're having problems, you can tweet to us [@expo](https://twitter.com/expo) or ask in our [forums](https://forums.expo.dev/c/expo-dev-tools/61) or [Discord](https://chat.expo.dev/).
+
+Snack is Open Source. You can find the code on the [GitHub repo](https://github.com/expo/snack).
+
